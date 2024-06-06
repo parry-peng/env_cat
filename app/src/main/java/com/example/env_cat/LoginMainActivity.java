1544df5f0d7ed2e9957c55ab086abf0786c76cee
Ychen-coder <y1c1c11c@163.com>
2024-06-06 21:30:13 +0800

    V0.1 init
    Signed-off-by: Ychen-coder <y1c1c11c@163.com>

--- app/src/main/java/com/example/env_cat/LoginMainActivity.java
+++ app/src/main/java/com/example/env_cat/LoginMainActivity.java
@@ -132,6 +132,8 @@ public class LoginMainActivity extends AppCompatActivity implements View.OnClick
                     // 设置启动标志：跳转到新页面时，栈中的原有实例都被清空，同时开辟新任务的活动栈
                     intent_login2main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent_login2main);
+                }else{
+                    Toast.makeText(LoginMainActivity.this, "登录失败,请联系管理员!", Toast.LENGTH_SHORT).show();
                 }
             } catch (JSONException e) {
                 Log.e(TAG, "解析服务器传来的JSon数据出错");
